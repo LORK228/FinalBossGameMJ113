486628b83a2c1be473bdaafe018ec1866427019b
LORK228 <85165063+LORK228@users.noreply.github.com>
2022-08-19 20:15:04 +0700

    menu

---
+++ Assets/Scripts/OptionMenu.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class OptionMenu : MonoBehaviour
+{
+    public AudioMixer AudioMixer;
+
+    public void SetVolume(float volume)
+    {
+        AudioMixer.SetFloat("Volume", Mathf.Log10(volume) * 20);
+    }
+}
