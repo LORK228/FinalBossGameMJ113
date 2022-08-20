0e8df5ac0a67348d0e2ff68c89854e742d289eca
LORK228 <85165063+LORK228@users.noreply.github.com>
2022-08-20 13:39:26 +0700

    sa

--- Assets/Scripts/TimeManager.cs
+++ Assets/Scripts/TimeManager.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class TimeManager : MonoBehaviour
 {
-    public bool isDo;
+    [HideInInspector] public bool isDo { private get; set; }
     private GameObject[] rewindInTimesObjects;
     private List<RewindInTime> rewindInTimes;
 
